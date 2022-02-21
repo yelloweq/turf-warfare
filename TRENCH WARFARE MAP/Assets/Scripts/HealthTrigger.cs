3ec3f32b4cfdc15f66a65c2f17b17b972bcfe35e
ayub1281 <ayubwahab71@gmail.com>
2022-02-21 00:56:14 +0000

    Added comments
    Added comments

--- TRENCH WARFARE MAP/Assets/Scripts/HealthTrigger.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/HealthTrigger.cs
@@ -20,11 +20,11 @@ public class HealthTrigger : MonoBehaviour
     
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Player" &&  userCurrency.getCurrency() >= 500 && userBase.health < 100)
+        if (other.gameObject.tag == "Player" &&  userCurrency.getCurrency() >= 500 && userBase.health < 100)//when player has >500 monney and <full health
         {
             message.gameObject.SetActive(true);
 
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E)) //increases health by 20 or max if base has taken less than 20 total damage
             {
                 if(userBase.health >= 80)
                 {
@@ -36,18 +36,18 @@ public class HealthTrigger : MonoBehaviour
                     userBase.health += 20;
                     healthbar.increaseHealth(20);
                 }
-                userCurrency.updateCurrency(-500);
+                userCurrency.updateCurrency(-500);//reduces currency by 500
             }
         }
-        else
+        else//Otherwise it displays the message 'unavailable'
         {
             message.text = "Unavailable";
             message.gameObject.SetActive(true);
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit(Collider other)//when moving away from buy station
+    {//resets message to its original incase it was changed to 'unavailable' and unactivates it for next time
         message.text = originalText;
         message.gameObject.SetActive(false);
     }
