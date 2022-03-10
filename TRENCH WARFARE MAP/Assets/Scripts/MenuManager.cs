274ce4def531adaafd67565dec3fca0947400811
fulm44 <180081772@aston.ac.uk>
2022-03-10 16:14:34 +0000

    Revert "new UI"
    This reverts commit f22a47883df060ce798ad69ef87bd9040a2bb4dd.

--- TRENCH WARFARE MAP/Assets/Scripts/MenuManager.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/MenuManager.cs
@@ -1,29 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using Photon.Pun;
-using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
 
-  public void PlayGame()
-  {
-    if (PhotonNetwork.LocalPlayer.IsMasterClient) // only lets host to start the game
+    public void PlayGame()
     {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-  }
 
+    public void PlayGameOffline()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 
-  public void PlayGameOffline()
-  {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-  }
-
-  public void QuitGame()
-  {
-    Application.Quit();
-  }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
