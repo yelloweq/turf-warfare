e18b542b79320325135722623cca91c8f933f9bc
ayub1281 <ayubwahab71@gmail.com>
2022-03-13 17:21:16 +0000

    Revert "Squashed commit of the following:"
    This reverts commit 23115cf5465dfc61e65c1fe18ec939e5fe3876fd.

--- TRENCH WARFARE MAP/Assets/Scripts/BaseHealth.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/BaseHealth.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class BaseHealth : MonoBehaviour
 {
-  //Starting health when bases spawn
-  public double health = 100;
+    //Starting health when bases spawn
+    public double health = 100;
 
-  public GameObject Explosion;
+    public GameObject Explosion;
 
-  public GameObject gameCompleteScreen;
+    public GameObject gameCompleteScreen;
 
-  public HealthbarScript HealthbarScript;
+    public HealthbarScript HealthbarScript;
 
-  public void OnCollisionEnter(Collision collision)
-  {
-    //If the ball collides with the base
-    if (collision.gameObject.tag == "ball")
-
+    public void OnCollisionEnter(Collision collision)
     {
-      //if the healthbar is assigned in inspector
-      if (HealthbarScript)
-      {
-        //Calls the damageTaken method within HealthbarScript script
-        HealthbarScript.damageTaken(34);
-        health -= 34;
-      }
+        //If the ball collides with the base
+        if (collision.gameObject.tag == "ball")
+
+        {
+            //if the healthbar is assigned in inspector
+            if (HealthbarScript)
+            {
+                //Calls the damageTaken method within HealthbarScript script
+                HealthbarScript.damageTaken(34);
+                health -= 34;
+            }
+        }
+
     }
 
-  }
-
-  private void Update()
-  {
-    //if the health is 0 or below
-    if (health <= 0)
+    private void Update()
     {
-      //Starts exploding particle effect
-      Destroy(Instantiate(Explosion, this.transform.position, this.transform.rotation), 2);
-      //Destroys the base prefab
-      Destroy(this.gameObject);
-      gameCompleteScreen.SetActive(true);
+        //if the health is 0 or below
+        if (health <= 0)
+        {
+            //Starts exploding particle effect
+            Destroy(Instantiate(Explosion, this.transform.position, this.transform.rotation), 2);
+            //Destroys the base prefab
+            Destroy(this.gameObject);
+            gameCompleteScreen.SetActive(true);
+        }
     }
-  }
 
 }
