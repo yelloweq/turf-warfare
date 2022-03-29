0af748b0eaf41617e860bf00fa115da2c8f8ec99
Ful <180081772@aston.ac.uk>
2022-03-29 15:28:42 +0100

    fixed errors

--- TRENCH WARFARE MAP/Assets/Scripts/ScoreElement.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/ScoreElement.cs
@@ -6,12 +6,14 @@ using TMPro;
 public class ScoreElement : MonoBehaviour
 {
 
-  public TMP_Text usernameText;
-  public TMP_Text winsText;
+    public TMP_Text usernameText;
+    public TMP_Text winsText;
+    public TMP_Text rank;
 
-  public void NewScoreElement(string _username, int _wins)
-  {
-    usernameText.text = _username;
-    winsText.text = _wins.ToString();
-  }
+    public void NewScoreElement(string _rank, string _username, int _wins)
+    {
+        rank.text = _rank;
+        usernameText.text = _username;
+        winsText.text = _wins.ToString();
+    }
 }
