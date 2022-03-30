6abf04b6aac4f7d272edfd03ccc479c1acf96156
AG1LL <gillaran0@gmail.com>
2022-03-30 20:24:02 +0100

    Base Health Fixed

--- TRENCH WARFARE MAP/Assets/Scripts/BaseHealth.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/BaseHealth.cs
@@ -4,64 +4,43 @@ using UnityEngine;
 
 public class BaseHealth : MonoBehaviour
 {
-  //Starting health when bases spawn
-  public double health = 100;
+    //Starting health when bases spawn
+    public double health = 100;
 
-  public GameObject Explosion;
+    public GameObject Explosion;
 
-  public GameObject gameCompleteScreen;
+    public GameObject gameCompleteScreen;
 
-  public HealthbarScript HealthbarScript;
+    public HealthbarScript HealthbarScript;
 
-  public CharacterCurrency currency;
-
-  public void Start()
-  {
-   
-  }
-
-  public void OnCollisionEnter(Collision collision)
-  {
-     currency = GameObject.Find("Player").GetComponent<CharacterCurrency>();
-    //If the ball collides with the base
-    if (collision.gameObject.tag == "ball") {
-      currency.updateCurrency(300);
-    }
-
-    if(currency)
+    public void OnCollisionEnter(Collision collision)
     {
-      
-      //dubug.log("Add money");
-    }
-    
+        //If the ball collides with the base
+        if (collision.gameObject.tag == "ball")
 
+        {
+            //if the healthbar is assigned in inspector
+            if (HealthbarScript)
+            {
+                //Calls the damageTaken method within HealthbarScript script
+                HealthbarScript.damageTaken(34);
+                health -= 34;
+            }
+        }
+
+    }
+
+    private void Update()
     {
-      //if the healthbar is assigned in inspector
-      if (HealthbarScript)
-      {
-        //Calls the damageTaken method within HealthbarScript script
-        HealthbarScript.damageTaken(34);
-        health -= 34;
-      }
+        //if the health is 0 or below
+        if (health <= 0)
+        {
+            //Starts exploding particle effect
+            Destroy(Instantiate(Explosion, this.transform.position, this.transform.rotation), 2);
+            //Destroys the base prefab
+            Destroy(this.gameObject);
+            gameCompleteScreen.SetActive(true);
+        }
     }
-
-  }
-
-  private void Update()
-  {
-    //if the health is 0 or below
-    if (health <= 0)
-    {
-      //Starts exploding particle effect
-      Destroy(Instantiate(Explosion, this.transform.position, this.transform.rotation), 2);
-      //Destroys the base prefab
-      Destroy(this.gameObject);
-      gameCompleteScreen.SetActive(true);
-    }
-  }
-
-  //public int GetHit(){
-
-//}
 
 }
