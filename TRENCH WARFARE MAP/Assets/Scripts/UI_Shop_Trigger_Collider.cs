1c200218388352b789c7e575a22582cbf3c29caa
AG1LL <gillaran0@gmail.com>
2022-03-30 03:40:20 +0100

    Revert "updated upgrade system"
    This reverts commit 792333446c2a94302a0ea58cb1af3a96aa87ab4e.

--- TRENCH WARFARE MAP/Assets/Scripts/UI_Shop_Trigger_Collider.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/UI_Shop_Trigger_Collider.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class UI_Shop_Trigger_Collider : MonoBehaviour
 {
-    public GameObject menuUI;
-    GameObject mouseLookScript;
-    public Text message;
 
-    private void Start()
+    private bool isMouseOverUI;
+
+    public GameObject menuUI;
+
+    private void Update()
     {
-        mouseLookScript = GameObject.FindWithTag("MainCamera");
+        isMouseOverUI = EventSystem.current.IsPointerOverGameObject();
     }
 
     void OnTriggerEnter(Collider Obj)
@@ -22,7 +22,6 @@ public class UI_Shop_Trigger_Collider : MonoBehaviour
             menuUI.SetActive(true);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-            mouseLookScript.GetComponent<MouseLook>().enabled = false;
         }
     }
  
@@ -33,8 +32,17 @@ public class UI_Shop_Trigger_Collider : MonoBehaviour
             menuUI.SetActive(false);
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-            mouseLookScript.GetComponent<MouseLook>().enabled = true;
-            message.gameObject.SetActive(false);
         }
     }
+
+    void baseUpgrade()
+    {
+
+    }
+
+    void buyHealth()
+    {
+
+    }
+
 }
