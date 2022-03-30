792333446c2a94302a0ea58cb1af3a96aa87ab4e
AG1LL <gillaran0@gmail.com>
2022-03-30 03:29:18 +0100

    updated upgrade system
    -Improved overall UI design of menu
    - Optimised scripts
    -

--- TRENCH WARFARE MAP/Assets/Scripts/UI_Shop_Trigger_Collider.cs
+++ TRENCH WARFARE MAP/Assets/Scripts/UI_Shop_Trigger_Collider.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_Shop_Trigger_Collider : MonoBehaviour
 {
-
-    private bool isMouseOverUI;
-
     public GameObject menuUI;
+    GameObject mouseLookScript;
+    public Text message;
 
-    private void Update()
+    private void Start()
     {
-        isMouseOverUI = EventSystem.current.IsPointerOverGameObject();
+        mouseLookScript = GameObject.FindWithTag("MainCamera");
     }
 
     void OnTriggerEnter(Collider Obj)
@@ -22,6 +22,7 @@ public class UI_Shop_Trigger_Collider : MonoBehaviour
             menuUI.SetActive(true);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+            mouseLookScript.GetComponent<MouseLook>().enabled = false;
         }
     }
  
@@ -32,17 +33,8 @@ public class UI_Shop_Trigger_Collider : MonoBehaviour
             menuUI.SetActive(false);
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+            mouseLookScript.GetComponent<MouseLook>().enabled = true;
+            message.gameObject.SetActive(false);
         }
     }
-
-    void baseUpgrade()
-    {
-
-    }
-
-    void buyHealth()
-    {
-
-    }
-
 }
---
+++ TRENCH WARFARE MAP/Assets/Scripts/WindRegion.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindRegion : MonoBehaviour
+{
+
+    float[] directionArray = new float[] {-1, 1};
+
+    private float windStrength;
+    private Vector3 windDirection;
+
+    public Image windArrow;
+    public Text windStrengthText;
+
+    public float setStrength()
+    {
+        windStrength = Random.Range(1, 100);
+        Debug.Log(windStrength);
+        return windStrength;
+    }
+
+    public Vector3 setDirection()
+    {
+        int index = Random.Range(0,2);
+        windDirection = new Vector3(directionArray[index], 0, 0);
+        Debug.Log(windDirection);
+        return windDirection;
+    }
+
+    public Vector3 getDirection()
+    {
+        return windDirection;
+    }
+
+    public float getStrength()
+    {
+        return windStrength;
+    }
+
+    public void setArrow(float direction)
+    {
+        if(direction > 0)
+        {
+            windArrow.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }else if(direction < 0)
+        {
+            windArrow.transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+    }
+
+    public void setStrengthText(float strength)
+    {
+        windStrengthText.text = strength.ToString();
+    }
+}
